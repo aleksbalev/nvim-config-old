092dca7668ae953ff7865cb12faff6406699cb38
Aleks B <aleks.balev7899@gmail.com>
2023-06-07 12:52:52 +0200

    added new commands

--- lua/master_nvim/remap.lua
+++ lua/master_nvim/remap.lua
@@ -23,10 +23,15 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 vim.keymap.set("n", "<leader>tl", "<C-w>v<C-w>l:terminal<CR>")
 vim.keymap.set("n", "<leader>tj", "<C-w>s<C-w>j:terminal<CR>10<C-w>-")
+
 vim.keymap.set("n", "<leader>fl", "<C-w>v<C-w>l<CR>")
+vim.keymap.set("n", "<leader>fh", "<C-w>v<C-w>h<CR>")
+vim.keymap.set("n", "<leader>fk", "<C-w>s<C-w>k<CR>")
+vim.keymap.set("n", "<leader>fj", "<C-w>s<C-w>j<CR>")
 
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+vim.keymap.set("n", "<leader>b", ":ls<CR>:b<space>")
